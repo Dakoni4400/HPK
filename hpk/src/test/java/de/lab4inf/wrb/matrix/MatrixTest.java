1762f148c42722d881a82d5f8ba4b011fc207f1d
Shayan Nosrat <44204942+shayannosrat@users.noreply.github.com>
2018-11-14 19:35:43 +0100

    Added 3 new tests for the split function

--- hpk/src/test/java/de/lab4inf/wrb/matrix/MatrixTest.java
+++ hpk/src/test/java/de/lab4inf/wrb/matrix/MatrixTest.java
@@ -44,6 +44,119 @@ public class MatrixTest {
 		assertTrue(test.equals(res));
 	}
 	
+		/**
+	 * Tests the split function for a quadratic Matrix
+	 */
+	@Test
+	public void testSplitQuadraticMatrix() {
+		
+		Matrix[][] res = new Matrix[2][2];
+		Matrix[][] test = new Matrix[2][2];
+		double[][] a = new double[][] {{1,2,3,4}, {2,3,4,5}, {1,2,3,4}, {2,3,4,5}};
+		
+		double [][] res1  = new double[][] {{1,2},{2,3}};
+		double [][] res2  = new double[][] {{3,4},{4,5}};
+		double [][] res3  = new double[][] {{1,2},{2,3}};
+		double [][] res4  = new double[][] {{3,4},{4,5}};
+		
+		Matrix A = new Matrix(a);
+		
+		res[0][0] = new Matrix(res1);
+		res[0][1] = new Matrix(res2);
+		res[1][0] = new Matrix(res3);
+		res[1][1] = new Matrix(res4);
+		
+		
+		test = DivideNConquerMultiplier.split(A);
+		
+		assertTrue(test[0][0].equals(res[0][0]));
+		assertTrue(test[0][1].equals(res[0][1]));
+		assertTrue(test[1][0].equals(res[1][0]));
+		assertTrue(test[1][1].equals(res[1][1]));
+	}
+	
+	/**
+	 * Tests the split function for a 5x4 Matrix
+	 */
+	@Test
+	public void testSplitOddMatrix5x4() {
+		
+		Matrix[][] res = new Matrix[2][2];
+		Matrix[][] test = new Matrix[2][2];
+		
+		double[][] a = new double[][] {{1,2,3,4}, {2,3,4,5}, {1,2,3,4}, {2,3,4,5}, {1,2,3,4}};
+		
+		double [][] res1  = new double[][] {{1,2,3},{2,3,4},{1,2,3}};
+		double [][] res2  = new double[][] {{4,0},{5,0},{4,0}};
+		double [][] res3  = new double[][] {{2,3,4},{1,2,3}};
+		double [][] res4  = new double[][] {{5,0},{4,0}};
+		
+		Matrix A = new Matrix(a);
+		
+		res[0][0] = new Matrix(res1);
+		res[0][1] = new Matrix(res2);
+		res[1][0] = new Matrix(res3);
+		res[1][1] = new Matrix(res4);
+		
+		
+		test = DivideNConquerMultiplier.split(A);
+		
+		/*
+		System.out.println("Erwartetes Ergebnis:\n" + res.toString());
+		System.out.println("Ergebnis: \n" + test[0][0].toString());
+		System.out.println("Ergebnis: \n" + test[0][1].toString());
+		System.out.println("Ergebnis: \n" + test[1][0].toString());
+		System.out.println("Ergebnis: \n" + test[1][1].toString());
+		*/
+		
+		assertTrue(test[0][0].equals(res[0][0]));
+		assertTrue(test[0][1].equals(res[0][1]));
+		assertTrue(test[1][0].equals(res[1][0]));
+		assertTrue(test[1][1].equals(res[1][1]));
+		
+	}
+	
+	/**
+	 * Tests the split function for a 4x5 Matrix
+	 */
+	@Test
+	public void testSplitOddMatrix4x5() {
+		
+		Matrix[][] res = new Matrix[2][2];
+		Matrix[][] test = new Matrix[2][2];
+		
+		double[][] a = new double[][] {{1,2,3,4,5}, {2,3,4,5,6}, {1,2,3,4,5}, {2,3,4,5,6}};
+		
+		double [][] res1  = new double[][] {{1,2,3},{2,3,4},{1,2,3}};
+		double [][] res2  = new double[][] {{4,5},{5,6},{4,5}};
+		double [][] res3  = new double[][] {{2,3,4},{0,0,0}};
+		double [][] res4  = new double[][] {{5,6},{0,0}};
+		
+		Matrix A = new Matrix(a);
+		
+		res[0][0] = new Matrix(res1);
+		res[0][1] = new Matrix(res2);
+		res[1][0] = new Matrix(res3);
+		res[1][1] = new Matrix(res4);
+		
+		
+		test = DivideNConquerMultiplier.split(A);
+		
+		/*
+		System.out.println("Erwartetes Ergebnis:\n" + res.toString());
+		System.out.println("Ergebnis: \n" + test[0][0].toString());
+		System.out.println("Ergebnis: \n" + test[0][1].toString());
+		System.out.println("Ergebnis: \n" + test[1][0].toString());
+		System.out.println("Ergebnis: \n" + test[1][1].toString());
+		*/
+			
+		assertTrue(test[0][0].equals(res[0][0]));
+		assertTrue(test[0][1].equals(res[0][1]));
+		assertTrue(test[1][0].equals(res[1][0]));
+		assertTrue(test[1][1].equals(res[1][1]));
+		
+	}
+	
 	@Test
 	public void testIdentitySerialMultiplication() {
 		double[][] a = new double[][] {{1,2,3,4}, {2,3,4,5}, {1,2,3,4}, {2,3,4,5}};
